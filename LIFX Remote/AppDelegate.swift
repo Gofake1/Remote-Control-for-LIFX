ef2e73f015c35cd358fe94b91aff1bc370e3795a
David Wu <david@gofake1.net>
2016-06-16 14:01:39 -0700

    Initial Commit

---
+++ LIFX Remote/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LIFX Remote
+//
+//  Created by David Wu on 6/16/16.
+//  Copyright © 2016 Gofake1. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
