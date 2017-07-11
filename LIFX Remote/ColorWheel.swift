d0830f79b49ea7ffe9b18736387770948ca7502f
David Wu <david@gofake1.net>
2017-07-11 15:08:14 -0700

    Refactored ColorWheel
    Removed ReactiveCocoa, clamps crosshair inside circle, draws brightness

--- LIFX Remote/ColorWheel.swift
+++ LIFX Remote/ColorWheel.swift
@@ -7,39 +7,44 @@
 //
 
 import Cocoa
-import ReactiveSwift
 
 typealias RGB = (r: UInt8, g: UInt8, b: UInt8)
 
 @IBDesignable
 class ColorWheel: NSControl {
-    
-    var selectedColor = NSColor.white {
-        didSet {
-            needsDisplay = true
-        }
-    }
+
+    static var blackImage: CGImage?
     static var colorWheelImage: CGImage?
-    private var crosshairLocation: CGPoint! {
-        didSet {
-            needsDisplay = true
-        }
-    }
+    private(set) var selectedColor = NSColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    private var brightness: CGFloat = 1.0
+    private var crosshairLocation: CGPoint!
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        if ColorWheel.blackImage == nil {
+            ColorWheel.blackImage = blackImage(rect: frame)
+        }
         if ColorWheel.colorWheelImage == nil {
-            ColorWheel.colorWheelImage = colorWheelImage(rect: frame, brightness: 1.0)
+            ColorWheel.colorWheelImage = colorWheelImage(rect: frame)
         }
         crosshairLocation = CGPoint(x: frame.width/2, y: frame.height/2)
     }
 
     override func draw(_ dirtyRect: NSRect) {
-        guard let context = NSGraphicsContext.current()?.cgContext else { return }
+        guard let context = NSGraphicsContext.current?.cgContext else { return }
 
         context.addEllipse(in: dirtyRect)
         context.clip()
         context.draw(ColorWheel.colorWheelImage!, in: dirtyRect)
+        context.setAlpha(1.0-brightness)
+        context.draw(ColorWheel.blackImage!, in: dirtyRect)
+        context.setAlpha(1.0)
+
+        if brightness < 0.5 {
+            context.setStrokeColor(CGColor.white)
+        } else {
+            context.setStrokeColor(CGColor.black)
+        }
         
         context.addEllipse(in: CGRect(origin: CGPoint(x: crosshairLocation.x-5.5, y: crosshairLocation.y-5.5),
                                       size: CGSize(width: 11, height: 11)))
@@ -50,17 +55,26 @@ class ColorWheel: NSControl {
         context.strokePath()
     }
 
-    private func colorWheelImage(rect: NSRect, brightness: CGFloat) -> CGImage {
-        let width = Int(rect.width), height = Int(rect.height)
-        var imageBytes = [RGB]()
-        for j in stride(from: height, to: 0, by: -1) {
-            for i in 0..<width {
-                let color = NSColor(coord: (i, j), center: (width/2, height/2), brightness: brightness)
-                imageBytes.append(RGB(r: UInt8(color.redComponent*255),
-                                      g: UInt8(color.greenComponent*255),
-                                      b: UInt8(color.blueComponent*255)))
-            }
-        }
+    func setColor(_ newColor: NSColor?) {
+        let center = CGPoint(x: frame.width/2, y: frame.height/2)
+        let newColor = newColor ?? NSColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        crosshairLocation = point(for: newColor, center: center)
+        selectedColor = newColor
+        brightness = newColor.scaledBrightness
+        needsDisplay = true
+    }
+
+    private func point(for color: NSColor, center: CGPoint) -> CGPoint? {
+        let h = color.hueComponent
+        let s = color.saturationComponent
+        let angle = h * 2 * CGFloat.pi
+        let distance = s * center.x
+        let x = center.x + sin(angle)*distance
+        let y = center.y + cos(angle)*distance
+        return CGPoint(x: x, y: y)
+    }
+
+    private func cgImage(bytes: inout [RGB], width: Int, height: Int) -> CGImage {
         return CGImage(width: width,
                        height: height,
                        bitsPerComponent: 8,
@@ -68,62 +82,72 @@ class ColorWheel: NSControl {
                        bytesPerRow: width * MemoryLayout<RGB>.size,
                        space: CGColorSpaceCreateDeviceRGB(),
                        bitmapInfo: CGBitmapInfo(rawValue: CGImageAlphaInfo.none.rawValue),
-                       provider: CGDataProvider(data: NSData(bytes: &imageBytes,
-                                                             length: imageBytes.count *
+                       provider: CGDataProvider(data: NSData(bytes: &bytes,
+                                                             length: bytes.count *
                                                                 MemoryLayout<RGB>.size))!,
                        decode: nil,
                        shouldInterpolate: false,
                        intent: .defaultIntent)!
     }
 
+    private func blackImage(rect: NSRect) -> CGImage {
+        let width = Int(rect.width), height = Int(rect.height)
+        var imageBytes = [RGB](repeating: RGB(r: 0, g: 0, b: 0), count: width * height)
+        return cgImage(bytes: &imageBytes, width: width, height: height)
+    }
+
+    private func colorWheelImage(rect: NSRect) -> CGImage {
+        let width = Int(rect.width), height = Int(rect.height)
+        var imageBytes = [RGB]()
+        for j in stride(from: height, to: 0, by: -1) {
+            for i in 0..<width {
+                let color = NSColor(coord: (i, j), center: (width/2, height/2), brightness: 1.0)
+                imageBytes.append(RGB(r: UInt8(color.redComponent*255),
+                                      g: UInt8(color.greenComponent*255),
+                                      b: UInt8(color.blueComponent*255)))
+            }
+        }
+        return cgImage(bytes: &imageBytes, width: width, height: height)
+    }
+
+    /// - postcondition: Triggers target-action
     private func setColor(at point: CGPoint) {
+        selectedColor = NSColor(coord: (Int(point.x), Int(point.y)),
+                                center: (Int(frame.width/2), Int(frame.height/2)),
+                                brightness: 1.0)
+        crosshairLocation = point
+        NSApp.sendAction(action!, to: target, from: self)
+    }
+
+    /// - returns: Clamped point and boolean indicating if the point was clamped
+    private func clamped(_ point: CGPoint) -> (CGPoint, Bool) {
         let centerX = frame.width/2
         let centerY = frame.height/2
-        selectedColor = NSColor(coord:  (x: Int(point.x), y: Int(point.y)),
-                                center: (x: Int(frame.width/2), y: Int(frame.height/2)),
-                                brightness: 1.0)
-
         let vX = point.x - centerX
         let vY = point.y - centerY
         let distanceFromCenter = sqrt((vX*vX) + (vY*vY))
         let radius = frame.width/2
         if distanceFromCenter > radius {
-            crosshairLocation = CGPoint(x: centerX + vX/distanceFromCenter * radius,
-                                        y: centerY + vY/distanceFromCenter * radius)
+            return (CGPoint(x: centerX + vX/distanceFromCenter * radius,
+                            y: centerY + vY/distanceFromCenter * radius), true)
         } else {
-            crosshairLocation = point
+            return (point, false)
         }
-
-        NSApp.sendAction(action!, to: target, from: self)
     }
 
     // MARK: - Mouse
     
     override func mouseDown(with event: NSEvent) {
-        setColor(at: convert(event.locationInWindow, from: nil))
+        let (clampedPoint, wasClamped) = clamped(convert(event.locationInWindow, from: nil))
+        if !wasClamped {
+            setColor(at: clampedPoint)
+            needsDisplay = true
+        }
     }
 
     override func mouseDragged(with event: NSEvent) {
-        setColor(at: convert(event.locationInWindow, from: nil))
-    }
-}
-
-extension NSColor {
-    convenience init(coord: (x: Int, y: Int), center: (x: Int, y: Int), brightness: CGFloat) {
-        let angle      = atan2(CGFloat(center.x - coord.x), CGFloat(center.y - coord.y)) + CGFloat.pi
-        let distance   = sqrt(pow(CGFloat(center.x - coord.x), 2) + pow(CGFloat(center.y - coord.y), 2))
-        let hue        = max(min(angle / (CGFloat.pi * 2), 1), 0)
-        let saturation = max(min(distance / CGFloat(center.x), 1), 0)
-        self.init(hue: hue, saturation: saturation, brightness: brightness, alpha: 1.0)
-    }
-}
-
-extension Reactive where Base: ColorWheel {
-    var selectedColorValue: BindingTarget<NSColor> {
-        return BindingTarget(on: UIScheduler(), lifetime: lifetime, action: { [weak base = self.base] value in
-            if let base = base {
-                base.selectedColor = value
-            }
-        })
+        let (clampedPoint, _) = clamped(convert(event.locationInWindow, from: nil))
+        setColor(at: clampedPoint)
+        needsDisplay = true
     }
 }
--- LIFX Remote/NSColor+.swift
+++ LIFX Remote/NSColor+.swift
@@ -9,6 +9,17 @@
 import Cocoa
 
 extension NSColor {
+
+    // Workaround: `NSColor`'s `brightnessComponent` is sometimes a value in [0-255] instead of in [0-1]
+    /// Brightness value scaled between 0 and 1
+    var scaledBrightness: CGFloat {
+        if brightnessComponent > 1.0 {
+            return brightnessComponent/255.0
+        } else {
+            return brightnessComponent
+        }
+    }
+    
     convenience init?(from color: LIFXLight.Color?) {
         if let color = color {
             self.init(hue:        CGFloat(color.hue)        / CGFloat(UInt16.max),
@@ -18,5 +29,14 @@ extension NSColor {
         } else {
             return nil
         }
+    }
+
+    convenience init(coord: (x: Int, y: Int), center: (x: Int, y: Int), brightness: CGFloat) {
+        let angle      = atan2(CGFloat(center.x - coord.x), CGFloat(center.y - coord.y)) + CGFloat.pi
+        let distance   = sqrt(pow(CGFloat(center.x - coord.x), 2) + pow(CGFloat(center.y - coord.y), 2))
+        self.init(hue:        max(min(angle / (CGFloat.pi * 2), 1), 0),
+                  saturation: max(min(distance / CGFloat(center.x), 1), 0),
+                  brightness: brightness,
+                  alpha:      1.0)
     }
 }
